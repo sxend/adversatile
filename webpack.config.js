1dc735ac40cbb5d4e4426f1e57644876d9c429c7
sxend <arimitsu.k+github@gmail.com>
2018-05-13 21:11:48 +0900

    add basic files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/Index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "adversatile.js"
+  }
+};
