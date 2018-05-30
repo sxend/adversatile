f4577a3f7274f124ecc73a9a545bb41ed15246fa
sxend <arimitsu.k+github@gmail.com>
2018-05-30 14:56:31 +0900

    refactor

--- src/Adversatile.ts
+++ src/Adversatile.ts
@@ -1,21 +1,16 @@
-import Contiguration from "./lib/Configuration";
-import { isObject } from "./lib/Misc";
 import main from "./lib/Main";
 
-async function absorber(...args: any[]) {
-  if (isObject(args[0]) && !!args[0].version) {
-    main(args[0]);
-  } else {
-    throw "abort";
-  }
-}
-
-const Adversatile = {
-  main: absorber
-};
+const Adversatile = { main };
 
 export default Adversatile;
 
-const __global = (0, eval)('this');
+async function expose() {
+  if (!!window && !("Adversatile" in window)) {
+    Object.defineProperty(window, "Adversatile", {
+      value: Adversatile,
+      writable: false
+    });
+  }
+}
 
-__global.Adversatile = __global.Adversatile || Adversatile;
+expose();
--- src/lib/Main.ts
+++ src/lib/Main.ts
@@ -1,4 +1,10 @@
 import Configuration from "./Configuration";
+import { isObject } from "./Misc";
 
-export default async function main(configuration: Configuration) {
+export default async function main(...args: any[]) {
+  if (isObject(args[0]) && !!args[0].version) {
+    // execute
+  } else {
+    throw "abort";
+  }
 }
