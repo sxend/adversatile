923fea48de89fea8023107e0ce2650229f0107b8
sxend <arimitsu.k+github@gmail.com>
2018-06-04 15:00:36 +0900

    fix macro test

--- test/lib/Macro.test.ts
+++ test/lib/Macro.test.ts
@@ -1,6 +1,7 @@
 import Macro from "../../src/lib/Macro";
+import Configuration from "../../src/lib/Configuration";
 
 test("Macro", done => {
-  const macro = new Macro();
+  const macro = new Macro(new Configuration());
   done();
 });
