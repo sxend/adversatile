1107337c6d9082298e33e8bf895137915f734b67
sxend <arimitsu.k+github@gmail.com>
2018-06-05 12:08:51 +0900

    define dispatcher interface

--- src/lib/Dispatcher.ts
+++ src/lib/Dispatcher.ts
@@ -1,10 +1,15 @@
 import { EventEmitter } from "events";
+import { IElementData } from "../../generated-src/protobuf/messages";
 
-export class Dispatcher extends EventEmitter {
-  dispatch(action: { event: string; data: any }) {
-    this.emit("dispatch", action);
+export interface IDispatcher {
+  dispatch(action: { event: "ElementData", data: IElementData }): void;
+  onDispatch(event: "ElementData", callback: (data: IElementData) => void): void;
+}
+export class Dispatcher extends EventEmitter implements IDispatcher {
+  dispatch(action: { event: string, data: any }): void {
+    this.emit(`dispatch:${action.event}`, action.data);
   }
-  onDispatch(callback: (action: { event: string; data: any }) => void) {
-    this.on("dispatch", callback);
+  onDispatch(event: string, callback: (data: any) => void): void {
+    this.on(`dispatch:${event}`, (data) => callback(data));
   }
 }
