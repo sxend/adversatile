988b8c06c7d217f57b168eaedcc18aa43ca5ba30
sxend <arimitsu.k+github@gmail.com>
2018-06-10 01:23:44 +0900

    sort class

--- src/lib/Configuration.ts
+++ src/lib/Configuration.ts
@@ -3,20 +3,37 @@ const deepmerge = require("deepmerge").default;
 
 export default class Configuration {
   version: number;
-  vm: ViewModelConf = new ViewModelConf();
   action: ActionConf = new ActionConf();
   store: StoreConf = new StoreConf();
-  ext: ExtConf = new ExtConf();
+  vm: ViewModelConf = new ViewModelConf();
 }
+
+export class ActionConf {
+  apiUrl: string = "/* @echo API_URL */";
+  jsonFetchPath: string = "/* @echo JSON_FETCH_PATH */" || "/demo/sample.json";
+  jsonPFetchPath: string = "/* @echo JSONP_FETCH_PATH */" || "/demo/sample.jsonp";
+}
+
+export class StoreConf { }
 
 export class ViewModelConf {
   selector: string = ".adversatile";
   markedClass: string = "adv-marked";
   deviceIfaAttrName: string = "data-adv-device-ifa";
   polling: PollingConf = new PollingConf();
-  em: ElementModelConf = new ElementModelConf();
   prefetch: PrefetchConf[] = [];
+  em: ElementModelConf = new ElementModelConf();
 }
+
+export class PollingConf {
+  interval: number = 100;
+}
+
+export class PrefetchConf {
+  name: string;
+  size: number;
+}
+
 export class ElementModelConf {
   nameAttributeName: string = "data-adv-em-name";
   templateQualifierKey: string = "data-adv-em-template";
@@ -30,32 +47,22 @@ export class ElementModelConf {
   templates: { [name: string]: string } = {};
   macro: MacroConf = new MacroConf();
 }
+
 export class ElementOption {
   constructor(public name: string) { }
   preRender: boolean = true;
   format: string = "native";
   assets: number[] = []; // FIXME use official asset
 }
-export class ActionConf {
-  apiUrl: string = "/* @echo API_URL */";
-  jsonFetchPath: string = "/* @echo JSON_FETCH_PATH */" || "/demo/sample.json";
-  jsonPFetchPath: string = "/* @echo JSONP_FETCH_PATH */" || "/demo/sample.jsonp";
-}
-export class StoreConf { }
+
 export class MacroConf {
   link: LinkMacroConf = new LinkMacroConf();
 }
+
 export class LinkMacroConf {
   selector: string = 'a[data-adv-macro-link]';
 }
-export class PollingConf {
-  interval: number = 100;
-}
-export class PrefetchConf {
-  name: string;
-  size: number;
-}
-export class ExtConf { }
+
 export function isConfiguration(obj: any): boolean {
   return !!obj && obj.version !== void 0;
 }
