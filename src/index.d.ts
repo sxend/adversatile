2ef2bbee680cd3fed0a83cfca7e1f524ab5b21c8
sxend <arimitsu.k+github@gmail.com>
2018-06-13 22:10:26 +0900

    remove unuse define

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,19 +7,3 @@ declare module "deepmerge" {
   }
   export default function deepmerge<T1, T2>(x: T1, y: T2, options?: Options): T1 & T2;
 }
-declare module "deepmerge/dist/es" {
-  interface Options {
-    clone?: boolean;
-    arrayMerge?(destination: any[], source: any[], options?: Options): any[];
-    isMergeableObject?(value: object): boolean;
-  }
-  export default function deepmerge<T1, T2>(x: T1, y: T2, options?: Options): T1 & T2;
-}
-declare module "deepmerge/dist/umd" {
-  interface Options {
-    clone?: boolean;
-    arrayMerge?(destination: any[], source: any[], options?: Options): any[];
-    isMergeableObject?(value: object): boolean;
-  }
-  export default function deepmerge<T1, T2>(x: T1, y: T2, options?: Options): T1 & T2;
-}
